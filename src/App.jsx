a3583530a2947a4af0121ad5a3eca6e62b001955
webdevhamid <abdulhamid1110000@gmail.com>
2025-10-12 20:04:55 +0600

    Added tailwindcss and daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import "./App.css";
-
 function App() {
-  return <h1>Blogify</h1>;
+  return (
+    <div>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
+  );
 }
 
 export default App;
